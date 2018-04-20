a8e3815225dcd72de2feb1f30c4298fcb4d43dec
agrha <agrhakartz@gmail.com>
2018-04-20 13:27:18 +0700

    checkpoint

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 // import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Rooms from './views/Rooms.vue'
+import HomePage from './views/HomePage.vue'
+// import Rooms2 from './views/Rooms2.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'homepage',
+      component: HomePage
+    },
+    {
+      path: '/rooms',
       name: 'rooms',
       component: Rooms
     },
---
+++ src/views/Rooms2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container jumbotron justify-content-center" id="body" >
+    </div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+#body {
+    background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsRxQOEzgrbawIx-sUnIhnV87KlkdDqFZVen3cyXS2erX3r96r);
+}
+</style>
